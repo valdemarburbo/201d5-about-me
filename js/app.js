2cc912d0ec099faee140d6c766221423d06aa5ad
Valdemar Burbo <valdemar.burbo@gmail.com>
2023-06-14 20:51:17 +0100

    Add while loop to prompt the user again if invalid answer is submitted

--- js/app.js
+++ js/app.js
@@ -6,78 +6,106 @@ alert("I have prepared a quiz to test your knowledge about me. Please answer the
 
 let quizScore = 0;
 
-let firstQuestion = prompt("Is the full version of my first name Voldemort?");
-switch (firstQuestion.toLowerCase()) {
-    case "yes":
-    case "y":
-        alert("Incorrect!");
-        break;
-    case "no":
-    case "n":
-        quizScore += 1;
-        alert("Correct!");
-        break;
-    default:
-        alert("Invalid input. Please use yes (y) or no (n) answers.")
+let firstQuestion = "";
+
+while (firstQuestion !== "y" && firstQuestion !== "yes" && firstQuestion !== "n" && firstQuestion !== "no") {
+
+    firstQuestion = prompt("Is the full version of my first name Voldemort?").toLowerCase();
+
+    switch (firstQuestion) {
+        case "yes":
+        case "y":
+            alert("Incorrect!");
+            break;
+        case "no":
+        case "n":
+            quizScore += 1;
+            alert("Correct!");
+            break;
+        default:
+            alert("Invalid input. Please use yes (y) or no (n) answers.")
+    }
 }
 
-let secondQuestion = prompt("Can I play the guitar?");
-switch (secondQuestion.toLowerCase()) {
-    case "yes":
-    case "y":
-        quizScore += 1; 
-        alert("Correct!");
-        break;
-    case "no":
-    case "n":
-        alert("Incorrect!");
-        break;
-    default:
-        alert("Invalid input. Please use yes (y) or no (n) answers.")
+let secondQuestion = "";
+
+while (secondQuestion !== "y" && secondQuestion !== "yes" && secondQuestion !== "n" && secondQuestion !== "no") {
+
+    secondQuestion = prompt("Can I play the guitar?").toLowerCase();
+
+    switch (secondQuestion) {
+        case "yes":
+        case "y":
+            quizScore += 1; 
+            alert("Correct!");
+            break;
+        case "no":
+        case "n":
+            alert("Incorrect!");
+            break;
+        default:
+            alert("Invalid input. Please use yes (y) or no (n) answers.")
+    }
 }
 
-let thirdQuestion = prompt("Do I hail from the UK?");
-switch (thirdQuestion.toLowerCase()) {
-    case "yes":
-    case "y": 
-        alert("Incorrect!");
-        break;
-    case "no":
-    case "n":
-        quizScore += 1;
-        alert("Correct!");
-        break;
-    default:
-        alert("Invalid input. Please use yes (y) or no (n) answers.")
+let thirdQuestion = "";
+
+while (thirdQuestion !== "y" && thirdQuestion !== "yes" && thirdQuestion !== "n" && thirdQuestion !== "no") {
+    thirdQuestion = prompt("Do I hail from the UK?").toLowerCase();
+
+    switch (thirdQuestion) {
+        case "yes":
+        case "y": 
+            alert("Incorrect!");
+            break;
+        case "no":
+        case "n":
+            quizScore += 1;
+            alert("Correct!");
+            break;
+        default:
+            alert("Invalid input. Please use yes (y) or no (n) answers.")    
+    }
 }
 
-let fourthQuestion = prompt("Is the colour of my eyes brown?")
-switch (fourthQuestion.toLowerCase()) {
-    case "yes":
-    case "y":
-        quizScore += 1; 
-        alert("Correct!");
-        break;
-    case "no":
-    case "n":
-        alert("Incorrect!");
-        break;
-    default:
-        alert("Invalid input. Please use yes (y) or no (n) answers.")
+let fourthQuestion = "";
+
+while (fourthQuestion !== "y" && fourthQuestion !== "yes" && fourthQuestion !== "n" && fourthQuestion !== "no") {
+    fourthQuestion = prompt("Is the colour of my eyes brown?").toLowerCase();
+
+    switch (fourthQuestion) {
+        case "yes":
+        case "y":
+            quizScore += 1; 
+            alert("Correct!");
+            break;
+        case "no":
+        case "n":
+            alert("Incorrect!");
+            break;
+        default:
+            alert("Invalid input. Please use yes (y) or no (n) answers.")
+    }
 }
-let fifthQuestion = prompt("Was I sold into slavery as a child and trained to be a fighter in an underground fight club, only to eventually fight my way out of it in a gruesome manner, leaving many bodies and my old life behind to start a new beginning by signing up to the Tech Educators Full-Stack MERN Developer course?");
-switch (fifthQuestion.toLowerCase()) {
-    case "yes":
-    case "y": 
-        alert("Incorrect!");
-        break;
-    case "no":
-    case "n":
-        quizScore += 1;
-        alert("Correct!");
-        break;
-    default:
-        alert("Invalid input. Please use yes (y) or no (n) answers.")
+
+let fifthQuestion = "";
+
+while (fifthQuestion !== "y" && fifthQuestion !== "yes" && fifthQuestion !== "n" && fifthQuestion !== "no") {
+    fifthQuestion = prompt("Was I sold into slavery as a child and trained to be a fighter in an underground fight club, only to eventually fight my way out of it in a gruesome manner, leaving many bodies and my old life behind to start a new beginning by signing up to the Tech Educators Full-Stack MERN Developer course?").toLowerCase();
+
+    switch (fifthQuestion.toLowerCase()) {
+        case "yes":
+        case "y": 
+            alert("Incorrect!");
+            break;
+        case "no":
+        case "n":
+            quizScore += 1;
+            alert("Correct!");
+            break;
+        default:
+            alert("Invalid input. Please use yes (y) or no (n) answers.")
+    }
 }
 
 alert("You got " + quizScore + " out of 5 questions right.")
